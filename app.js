fc8de4b3ca6180448a68d1b222e012b97c19dfc5
Idea-JTH <str.taehyun@gmail.com>
2020-08-06 00:50:24 +0900

    프리뷰 박스 추가 완료

--- app.js
+++ app.js
@@ -4,13 +4,29 @@ const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 const saveBtn = document.getElementById("jsSave");
+const color_status = document.getElementById("color__status");
+const line_canvas = document.getElementById("line__canvas");
+const line_ctx = line_canvas.getContext("2d");
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
+const LINE_SIZE = 100;
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
+// ctx말고 캔버스 자체에 크기값을 넣어주자
+line_canvas.width = LINE_SIZE;
+line_canvas.height = LINE_SIZE;
+line_ctx.strokeStyle = INITIAL_COLOR;
+line_ctx.lineWidth = 2.5; 
+line_ctx.fillStyle = "white";
+
+line_ctx.beginPath();
+line_ctx.moveTo(20, 50);
+line_ctx.lineTo(80, 50);
+// line_ctx.lineTo(80, 50);
+line_ctx.stroke();
 ctx.fillStyle = "white";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.strokeStyle = INITIAL_COLOR;
@@ -48,19 +64,40 @@ function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
     ctx.fillStyle = color;
+    color_status.style.backgroundColor=event.target.style.backgroundColor;
+    checkTextColor();
 }
 
 function handleRangeChange(event) {
     ctx.lineWidth = event.target.value;
+
+    //선 굵기를 변경할 시 우측 상단의 선 굵기 프리뷰가 변화함..
+    //코드 변경이 필요한 구역
+    line_ctx.lineWidth = event.target.value;
+    line_ctx.fillRect(0, 0, line_canvas.width, line_canvas.height);
+    line_ctx.stroke();
+}
+
+function checkTextColor() {
+    if(color_status.style.backgroundColor == "white") {
+        console.log("asdf");
+        color_status.style.color = "black";
+    } else {
+        color_status.style.color="white";
+    }
 }
 
 function handleModeClick(event) {
     if(filling) {
         filling = false;
         mode.innerText = "Paint"
+        
+        color_status.innerText ="Paint";
     } else {
         filling = true;
         mode.innerText = "Fill"
+        
+        color_status.innerText ="Fill";
     }
 }
 
@@ -100,7 +137,7 @@ if(colors) {
 }
 
 if(range) {
-    range.addEventListener("change", handleRangeChange);
+    range.addEventListener("input", handleRangeChange);
 }
 
 if(mode) {
